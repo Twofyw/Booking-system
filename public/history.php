7e33b97c2d4db5a39e95d42856b1eab6cebab9dc
Twofyw Yang <ywywywx@gmail.com>
2017-09-26 17:12:46 +0800

    Revert "Illustration upload"
    This reverts commit 795ae5b922b8dfbc4262b4baf770f287f2be7de4.

--- public/history.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // configuration
-    require("../includes/config.php");
-	$historys = CS50::query("SELECT * FROM history WHERE user_id = ?", $_SESSION["id"]);
-	render("history.php", ["historys" => $historys, "title" => "History"]);
-?>
--- views/price.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $price = number_format($_SESSION["stock"]["price"],2);
-    printf("A share of {$_SESSION["stock"]["name"]} ({$_SESSION["stock"]["symbol"]}) costs <strong>\${$price}</strong>.");
-?>
